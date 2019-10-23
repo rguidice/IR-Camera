e5f7fe6c2f2f299339f4cf12e39463c5dda5cc8b
Ryan Guidice <rjgnospam@gmail.com>
2019-10-22 21:56:58 -0600

    Each IO.py run creates directory with date&time with each video being named as timestamp.

--- IO.py
+++ IO.py
@@ -13,60 +13,71 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 import cv2
+import os
 
-IR_pin = 35
-vid_count = 0
+class irCam:
+    def __init__(self):
+        self.IR_pin = 35
+        self.vid_count = 1
+        
+        #initialize GPIO settings
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.IR_pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+        
+        #create directory named with date and time to store video files in
+        self.cwd = os.getcwd()
+        now = datetime.now()
+        self.video_dir = now.strftime("%m-%d-%Y_%H:%M:%S")
+        os.mkdir(self.video_dir)
+        
+        print("-------------------------------------------------------")
 
-def init():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
-    GPIO.setup(IR_pin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-    print("-------------------------------------------------------")
+    def main(self):
+        while True:
+            value = GPIO.input(self.IR_pin)
+            print(value)
+            if value != 0:
+                self.record()
+                time.sleep(5)
+            else:
+                print("nothing found")
+                time.sleep(1)
+                
+    def record(self):
+        cam = cv2.VideoCapture(0)
+        
+        #Create video codec and create VideoWriter object (AVI file format, 640x480 resolution, 10fps)
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        curr_time = datetime.now().strftime("_%H:%M:%S")
+        output_str = str(self.vid_count) + curr_time + '.avi'
+        output = cv2.VideoWriter(output_str,fourcc,10.0,(640,480))
+        
+        #Get starting time for video time comparison
+        origTime = time.process_time()
+        print("Camera recording")
 
-def main():
-    while True:
-        value = GPIO.input(IR_pin)
-        print(value)
-        if value != 0:
-            record()
-            time.sleep(5)
-        else:
-            print("nothing found")
-            time.sleep(1)
-            
-def record():
-    cam = cv2.VideoCapture(0)
-    global vid_count
-    
-    #Create video codec and create VideoWriter object (AVI file format, 640x480 resolution, 10fps)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    output_str = 'output' + str(vid_count) + '.avi'
-    output = cv2.VideoWriter(output_str,fourcc,10.0,(640,480))
-    
-    #Get starting time for video time comparison
-    origTime = time.process_time()
-    print("Camera recording")
-
-    #Record for about 25 seconds (origTime + 8)
-    while(cam.isOpened()):
-        res, frame = cam.read()
-        if res == True:
-            output.write(frame)
-            cv2.imshow("camera", frame)
-            currTime = time.process_time()
-            if currTime > (origTime + 5):
+        #Record for about 25 seconds (origTime + 8)
+        while(cam.isOpened()):
+            res, frame = cam.read()
+            if res == True:
+                output.write(frame)
+                cv2.imshow("camera", frame)
+                currTime = time.process_time()
+                if currTime > (origTime + 5):
+                    break
+            else:
                 break
-        else:
-            break
 
-    print("Camera done recording")
-    cam.release()
-    output.release()
-    cv2.destroyAllWindows()
-    vid_count = vid_count+1
-    return
+        print("Camera done recording")
+        cam.release()
+        output.release()
+        cv2.destroyAllWindows()
+        self.vid_count = self.vid_count+1
+        os.rename(self.cwd+"/"+output_str, self.cwd+"/"+self.video_dir+"/"+output_str)
+        return
 
-init()
-main()
-GPIO.cleanup()
+irCam1 = irCam()
+irCam1.main()
